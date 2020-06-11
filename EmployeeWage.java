2815c6ba8abcad3f134cb340fe96edf5038c1e22
JituShinde77 <shindejitu77@gmail.com>
2020-06-12 00:03:25 +0530

    Added: Employee Atendence Functionality.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,18 @@
 public class EmployeeWage{
 
+	private static final int PRESENT = 1;
+	private static final int ABSENT = 0;
+	//private final int isPresent;
+	public String checkAttendece(){
+		int empCheck = (int)Math.floor(Math.random() * 10 ) %2;
+		if (empCheck == PRESENT )
+			return "Present";
+		else
+			return "Absent";
+	}
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage..");
+		EmployeeWage empWage = new EmployeeWage();
+		System.out.println("Employee is: "+ empWage.checkAttendece());
 	}
 }
