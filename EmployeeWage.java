a693675f139abc6a3867344dc480810ecf89db59
JituShinde77 <shindejitu77@gmail.com>
2020-06-12 11:44:05 +0530

    Added: Total wage with condition max working hrs funcitonality.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,12 +8,14 @@ public class EmployeeWage{
 	private final int dayHrs;
 	private final int partTimeHrs;
 	private final int maxWorkingDays;
+	private final int maxWorkingHrs;
 
-	public EmployeeWage(int wagePerHour, int dayHrs, int partTimeHrs, int maxWorkingDays){
+	public EmployeeWage(int wagePerHour, int dayHrs, int partTimeHrs, int maxWorkingDays, int maxWorkingHrs){
       		this.wagePerHour = wagePerHour;
      		this.dayHrs = dayHrs;
 		this.partTimeHrs = partTimeHrs;
 		this.maxWorkingDays = maxWorkingDays;
+		this.maxWorkingHrs = maxWorkingHrs;
    	}
 
    	public int getEmpHrs(){
@@ -30,19 +32,23 @@ public class EmployeeWage{
   	 public int getTotalEmplyeeWage(){
 		int totalWage = 0;
 		int workingDays = 0;
-		while (workingDays < 20){
+		int workingHrs = 0;
+		while (workingDays < 20 && workingHrs < 100){
    			int empHrs = getEmpHrs();
      			int dailyWage = 0;
       			dailyWage = wagePerHour * empHrs;
 			totalWage = totalWage + dailyWage;
 			++workingDays;
+			workingHrs = workingHrs + empHrs;
 		}
+		if (workingHrs > maxWorkingHrs)
+			return wagePerHour * maxWorkingHrs;
       		return totalWage;
    	}
 
    	public static void main(String[] args){
    		System.out.println("Welcome to Employee Wage..");
-    		EmployeeWage empWage = new EmployeeWage(20, 8, 4, 20);
+    		EmployeeWage empWage = new EmployeeWage(20, 8, 4, 20, 100);
       		System.out.println("Employee Total Employee wage for month is: "+empWage.getTotalEmplyeeWage());
    	}
 }
