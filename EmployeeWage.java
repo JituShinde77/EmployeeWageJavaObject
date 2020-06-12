9a442ab5bdbd95eac0de2c5f2007e918dffaaa87
JituShinde77 <shindejitu77@gmail.com>
2020-06-12 11:15:17 +0530

    Added: Part Time Emplyee Functionalily.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,34 +2,37 @@ public class EmployeeWage{
 
 	private static final int PRESENT = 1;
 	private static final int ABSENT = 0;
+	private static final int PARTTIME = 2;
 
 	private final int wagePerHour;
-	private final int dayHours;
+	private final int dayHrs;
+	private final int partTimeHrs;
 
-	public EmployeeWage(int wagePerHour, int dayHours){
+	public EmployeeWage(int wagePerHour, int dayHrs, int partTimeHrs){
                 this.wagePerHour = wagePerHour;
-		this.dayHours = dayHours;
+                this.dayHrs = dayHrs;
+		this.partTimeHrs = partTimeHrs;
         }
 
-	public int checkAttendence(){
-                int empCheck = (int)Math.floor(Math.random() * 10 ) %2;
+        public int getEmpHrs(){
+                int empCheck = (int)Math.floor(Math.random() * 10 ) %3;
                 if (empCheck == PRESENT )
-                        return PRESENT;
-                else
-                        return ABSENT;
+                        return dayHrs;
+                else if (empCheck == PARTTIME)
+                        return partTimeHrs;
+		else
+			return 0;
         }
 
-	public int getDailyEmplyeeWage(){
-		int isPresent = checkAttendence();
-		int dailyWage = 0;
-		if (isPresent == 1){
-			dailyWage = wagePerHour * dayHours;
-		}
-		return dailyWage;
-	}
-	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage..");
-		EmployeeWage empWage = new EmployeeWage(20, 8);
-		System.out.println("Employee Daily wage is: "+empWage.getDailyEmplyeeWage());
-	}
+        public int getDailyEmplyeeWage(){
+                int empHrs = getEmpHrs();
+                int dailyWage = 0;
+                dailyWage = wagePerHour * empHrs;
+                return dailyWage;
+        }
+        public static void main(String[] args){
+                System.out.println("Welcome to Employee Wage..");
+                EmployeeWage empWage = new EmployeeWage(20, 8, 4);
+                System.out.println("Employee Daily wage is: "+empWage.getDailyEmplyeeWage());
+        }
 }
